dc79c49008afa3be656b8a9af0d3d2dd508ff2fe
Roboam Casanova Castro <zr.rollo@gmail.com>
2021-03-08 11:41:55 -0600

    A solution for the exercise

--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/RolloCasanova/bootcamp-challenge-w3/shape"
 )
 
 func main() {
 	// create a new Square, Circle and Hexagon
+	square := shape.NewSquare("S", "Square", 4)
+	circle := shape.NewCircle("C", "Circle", 10.2)
+	hexagon := shape.NewHexagon("H", "Hexagon", 5)
 
 	// print shapes
+	printShape(square)
+	printShape(circle)
+	printShape(hexagon)
 
 	// change shape's value
+	square.NewName("Bob")
+	square.NewShapeType("Tilted rombus")
+	square.NewSide(8.88)
+
+	circle.NewName("CJ")
+	circle.NewShapeType("Circulo")
+	circle.NewRadius(1.01)
+
+	hexagon.NewName("0x00")
+	hexagon.NewShapeType("Bee's favorite shape")
+	hexagon.NewSide(0.25)
 
 	// print shapes again
+	printShape(square)
+	printShape(circle)
+	printShape(hexagon)
 }
 
 func printShape(s shape.Shape) {
-	// print shape information
+	fmt.Println(s.Info())
 }
--- shape/shape.go
+++ shape/shape.go
@@ -2,4 +2,23 @@ package shape
 
 // Shape interface definition
 type Shape interface {
+	Area() float64
+	Perimeter() float64
+	Info() string
+}
+
+// shape struct definition
+type shape struct {
+	name      string
+	shapeType string
+}
+
+// NewName sets a new name to shape s
+func (s *shape) NewName(n string) {
+	s.name = n
+}
+
+// NewShapeType sets a new FigureType to shape s
+func (s *shape) NewShapeType(st string) {
+	s.shapeType = st
 }
